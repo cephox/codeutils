c412da3f666e724b085d1021585fd22dce4de72f
cephox <paul-stier@gmx.de>
2020-10-23 15:45:28 +0200

    Added color.h

---
+++ C/ColoredOutput/colors.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLD_BLACK   "\033[1m\033[30m"      /* BOLD Black */
+#define BOLD_RED     "\033[1m\033[31m"      /* BOLD Red */
+#define BOLD_GREEN   "\033[1m\033[32m"      /* BOLD Green */
+#define BOLD_YELLOW  "\033[1m\033[33m"      /* BOLD Yellow */
+#define BOLD_BLUE    "\033[1m\033[34m"      /* BOLD Blue */
+#define BOLD_MAGENTA "\033[1m\033[35m"      /* BOLD Magenta */
+#define BOLD_CYAN    "\033[1m\033[36m"      /* BOLD Cyan */
+#define BOLD_WHITE   "\033[1m\033[37m"      /* BOLD White */
+
