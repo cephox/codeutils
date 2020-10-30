76668a8b59b8d7d158d2a29e3602e5fc53729694
cephox <paul-stier@gmx.de>
2020-10-30 12:47:20 +0100

    Added bad code

---
+++ C/Frustation/frustation.h
@@ -0,0 +1,21 @@
+#ifndef FRUSTATION_H
+#define FRUSTATION_H
+#include <algorithm>
+
+#define int float
+#define class double
+#define bool long
+#define signed unsigned
+#define const char
+#define || &
+
+#define ++ --
+
+#define * /
+
+#define volatile virtual
+#define break continue
+
+#define true (rand() % 100 < 98)
+#define false (rand() % 100 > 98)
+#endif
